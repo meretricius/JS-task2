b17f56441f2494f631850f805d9a272e373f1d9c
= <=>
2023-10-19 11:47:22 +0500

    task is completed

---
+++ index.js
@@ -0,0 +1,30 @@
+let name = prompt('Enter your name');
+let age = +prompt('Enter your age')
+let money = +prompt('How much money is available?')
+let clubCapacity = 10
+let guests = 0
+if(name.charAt().toLowerCase() === "a" && age <= 40 && money > 100) {
+    alert("Welcome, " + name + "!");
+  } else {
+    alert("Please leave the page "+ name);
+  }
+
+
+
+expences = 0
+balace = 0
+moneyTwo = 10000
+let nameTwo = prompt('Whats your name?').toLowerCase().trim() 
+    if(nameTwo === 'alex') {
+        account = +prompt('Account password')
+        if(account === 7777) {
+            expences = +prompt('How much to cash out?')
+            if(expences <= moneyTwo) {
+                alert(balance = moneyTwo - expences  )
+            }
+        }
+    } else {
+        alert('undefined user')
+    }
+    console.log(balace);
+
